38bcd2fb4175f7239e1e48a470b71fcde153d6cc
Milan2496 <mg101@kent.ac.uk>
2026-02-06 12:11:24 +0000

    add member

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,23 +1,21 @@
 n = ["Picard", "Riker", "Data", "Worf", "La Forge"]
-r = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant Commander"]
+r = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant", "Lieutenant Commander"]
 d = ["Command", "Command", "Operations", "Security", "Engineering"]
 id = ["SF-1701-A", "SF-1701-B", "SF-1701-C", "SF-1701-D", "SF-1701-E" ]
-
 
 
 def init_database():
     print("BOOTING SYSTEM...")
     print("...")
     print("WELCOME TO FLEET COMMAND")
-
     print("\nCurrent Crew List:")           
     for i in range(len(n)):
         print( n[i] + " - " + r[i] + " - " + d[i] + " - " + id[i])
 
+
 def display_menu():
     name = input("\nWhat is your full name? ")
     print("USER " + name + " HAS LOGGED IN...")
-
     print("\n--- MENU ---")
     print("1. Add Member")
     print("2. Remove Member")
@@ -27,12 +25,38 @@ def display_menu():
     print("6. Filter by Division")
     print("7. Calculate Payroll")
     print("8. Count Officers")
-
     opt = int(input("Select option: "))
     return opt
 
+def add_member(n, r, d, id):
+
+    rank = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant"]
+
+    new_name = input("Name: ")
+    new_rank = input("Rank: ")
+    new_rank = new_rank.title()
+
+    while new_rank not in rank:
+        new_rank = input("This is not a valid TNG Rank. Please enter a valid Rank: ")
+        new_rank = new_rank.title()
 
 
+    new_div = input("Division: ")   
+    new_id = input("ID: ")      
+
+    while new_id in id:
+        new_id = input("This is not a unique ID. Please enter a unique ID: ")
+
+    n.append(new_name)
+    r.append(new_rank)
+    d.append(new_div)
+    id.append(new_id)
+
+    print("\nUpdated Crew List:")
+
+    for i in range(len(n)):
+            print( n[i] + " - " + r[i] + " - " + d[i] + " - " + id[i])
+    return n, r, d, id
 
 
 def main():
@@ -40,9 +64,11 @@ def main():
     opt = display_menu()
 
     if opt == 1:
-        print("hello")
+        add_member(n, r, d, id)
+        
 
 
+        
 
 
 main()
