27a7714a41c223d8f5ed7896c26cafda081030a8
Milan2496 <mg101@kent.ac.uk>
2026-02-06 12:25:29 +0000

    remove members

--- fleet_manager.py
+++ fleet_manager.py
@@ -25,35 +25,43 @@ def display_menu():
     print("6. Filter by Division")
     print("7. Calculate Payroll")
     print("8. Count Officers")
-    opt = int(input("Select option: "))
+    opt = int(input("\nSelect option: "))
     return opt
 
 def add_member(n, r, d, id):
-
     rank = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant"]
-
     new_name = input("Name: ")
     new_rank = input("Rank: ")
     new_rank = new_rank.title()
-
     while new_rank not in rank:
         new_rank = input("This is not a valid TNG Rank. Please enter a valid Rank: ")
         new_rank = new_rank.title()
-
-
     new_div = input("Division: ")   
     new_id = input("ID: ")      
-
     while new_id in id:
         new_id = input("This is not a unique ID. Please enter a unique ID: ")
-
     n.append(new_name)
     r.append(new_rank)
     d.append(new_div)
     id.append(new_id)
-
     print("\nUpdated Crew List:")
+    for i in range(len(n)):
+            print( n[i] + " - " + r[i] + " - " + d[i] + " - " + id[i])
+    return n, r, d, id
 
+
+def remove_members(n, r, d, id):
+    rem = input("Name to remove: ")
+    if rem in n:           
+        idx = n.index(rem)
+        n.pop(idx)
+        r.pop(idx)
+        d.pop(idx)
+        id.pop(idx)
+        print("Crew member removed")
+    else:
+        print("Name not found")
+    print("\nUpdated Crew List:")
     for i in range(len(n)):
             print( n[i] + " - " + r[i] + " - " + d[i] + " - " + id[i])
     return n, r, d, id
@@ -65,6 +73,10 @@ def main():
 
     if opt == 1:
         add_member(n, r, d, id)
+
+    elif opt == 2:
+        remove_members(n, r, d, id)
+        
         
 
 
