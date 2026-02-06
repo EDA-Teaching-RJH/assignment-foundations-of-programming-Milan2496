66d9da9d5e1ea204abc3bc7efe3bc393fdc163fc
Milan2496 <mg101@kent.ac.uk>
2026-02-06 11:40:43 +0000

    display_menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,6 +4,7 @@ d = ["Command", "Command", "Operations", "Security", "Engineering"]
 id = ["SF-1701-A", "SF-1701-B", "SF-1701-C", "SF-1701-D", "SF-1701-E" ]
 
 
+
 def init_database():
     print("BOOTING SYSTEM...")
     print("...")
@@ -13,12 +14,34 @@ def init_database():
     for i in range(len(n)):
         print( n[i] + " - " + r[i] + " - " + d[i] + " - " + id[i])
 
+def display_menu():
+    name = input("\nWhat is your full name? ")
+    print("USER " + name + " HAS LOGGED IN...")
+
+    print("\n--- MENU ---")
+    print("1. Add Member")
+    print("2. Remove Member")
+    print("3. Update Rank")
+    print("4. Display Roster")
+    print("5. Search Crew")
+    print("6. Filter by Division")
+    print("7. Calculate Payroll")
+    print("8. Count Officers")
+
+    opt = int(input("Select option: "))
+    return opt
+
 
 
 
 
 def main():
     init_database()
+    opt = display_menu()
+
+    if opt == 1:
+        print("hello")
+
 
 
 
